97bb02dd646fc09af90caacb95b8554906a12356
ne_Sachirou <utakata.c4se@gmail.com>
2017-12-31 15:18:36 +0900

    `mix new extender --sup --module ExTendEr --app extender`

---
+++ lib/extender.ex
@@ -0,0 +1,18 @@
+defmodule ExTendEr do
+  @moduledoc """
+  Documentation for ExTendEr.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExTendEr.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/extender_test.exs
@@ -0,0 +1,8 @@
+defmodule ExTendErTest do
+  use ExUnit.Case
+  doctest ExTendEr
+
+  test "greets the world" do
+    assert ExTendEr.hello() == :world
+  end
+end
